1df022e6b9ff48e12c8204af43a1809e4c43ec86
Mohamed Mohamed El Dehairy <m.eldehairy@gmail.com>
2019-08-04 14:19:30 +0200

    Simplified a bit DefaultAPIClientTest case and trying fix unit tests on iOS versions before iOS 12

--- Adyen/AdyenTests/ClientAPI/DefaultAPIClientTest.swift
+++ Adyen/AdyenTests/ClientAPI/DefaultAPIClientTest.swift
@@ -24,14 +24,13 @@ class DefaultAPIClientTest: XCTestCase {
             PlaceModel(id: "4f5f4a63e4b0028cfa222448", name: "CineMeerse", coordinate: CLLocationCoordinate2D(latitude: 52.30175696243503, longitude: 4.69554693601281), category: "Movie Theater"),
             PlaceModel(id: "581483c738fa25270f6a022d", name: "Jopenkerk Hoofddorp", coordinate: CLLocationCoordinate2D(latitude: 52.30211291653203, longitude: 4.6887681648385815), category: "Brewery")
         ]
-        let path = Bundle(for: PlaceModelParserTest.self).path(forResource: "test_places_success", ofType: "json")!
-        let url = URL(fileURLWithPath: path)
-        let expectedData = try! Data(contentsOf: url)
         executer.onExecute = { _, completion in
-            completion(.success(expectedData), nil)
+            completion(.success(Data()), nil)
             return NullCancelable()
         }
-        let resource = APIResource(request: URLRequest(url: URL(string: "https://www.google.com")!), parser: parse)
+        let resource = APIResource(request: URLRequest(url: URL(string: "https://www.google.com")!), parser: { _ in
+            return .success(expectedPlaces)
+        })
         let expectation = XCTestExpectation(description: "Expect DefaultAPIClient completion closure to be called")
         sut.fetch(resource: resource, deliverOn: DispatchQueue.main) { result in
             XCTAssertEqual(result.value, expectedPlaces)
